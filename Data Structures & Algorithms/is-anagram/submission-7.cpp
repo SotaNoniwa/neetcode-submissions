d201397f05fdfd5e66b452c3c112ab861358271e
Sota Noniwa <99335304+SotaNoniwa@users.noreply.github.com>
2026-06-13 19:40:38 +0900

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        unordered_map<char, int> count_s;
+        unordered_map<char, int> count_t;
+        for (int i; i < s.length(); i++) {
+            count_s[s[i]]++;
+            count_t[t[i]]++;
+        }
+        return count_s == count_t;
+    }
+};
