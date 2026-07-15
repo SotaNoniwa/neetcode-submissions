18b72af230ca87dd4e8864fe913f51be63540a00
Sota Noniwa <99335304+SotaNoniwa@users.noreply.github.com>
2026-07-15 09:27:05 +0900

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> left;
+        for (const char& c : s) {
+            if (!left.empty() && c == ')') {
+                if (left.top() != '(') return false;
+                left.pop();
+            } else if (!left.empty() && c == '}') {
+                if (left.top() != '{') return false;
+                left.pop();
+            } else if (!left.empty() && c == ']') {
+                if (left.top() != '[') return false;
+                left.pop();
+            } else {
+                left.push(c);
+            }
+        }
+        return left.empty();
+    }
+};
